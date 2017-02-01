0105f0f7f35dc2ef8b42720802ab1627d26e6cc5
Gabor Raz <gaborraz@hotmail.com>
2017-02-01 18:19:14 +0100

    remove ava

--- app/client/modules/core/components/tests/home.spec.js
+++
@@ -1,9 +0,0 @@
-import test from 'ava';
-import React from 'react';
-import { shallow } from 'enzyme';
-import Component from '../home';
-
-test('title', (t) => {
-  const wrapper = shallow(<Component />);
-  t.truthy(wrapper);
-});
