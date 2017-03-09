ecaee2fdcc6d3ac97877b22aed161fae1ae1a9ea
Gabor Raz <gaborraz@hotmail.com>
2017-03-09 12:26:54 +0100

    add login test

--- app/tests/firstSpec.js
+++
@@ -1,8 +0,0 @@
-describe('Chimp Mocha', function () {
-  describe('Page title', function () {
-    it('should be set by the Meteor method @watch', function () {
-      browser.url('http://localhost:3000');
-      expect(browser.getTitle()).to.equal('');
-    });
-  });
-});
