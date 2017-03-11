381f2c85f796aa52d957c41c1494a893d870635a
Gabor Raz <gaborraz@hotmail.com>
2017-03-11 18:26:33 +0100

    move schema definition into schema file

--- app/lib/collections/translations.js
+++ app/lib/collections/translations.js
@@ -1,28 +1,8 @@
 import { Mongo } from 'meteor/mongo';
-import SimpleSchema from 'simpl-schema';
+import TranslationsSchema from '../schemas/translations';
 
 const Translations = new Mongo.Collection('translations');
-Translations.schema = new SimpleSchema({
-  _id: {
-    type: String,
-  },
-  value_de: {
-    type: String,
-    optional: true,
-  },
-  value_en: {
-    type: String,
-    optional: true,
-  },
-  value_fr: {
-    type: String,
-    optional: true,
-  },
-  value_it: {
-    type: String,
-    optional: true,
-  },
-});
+Translations.schema = TranslationsSchema;
 Translations.attachSchema(Translations.schema);
 
 export default Translations;
